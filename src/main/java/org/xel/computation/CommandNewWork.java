e47315c2fcb134f19f087eeac0b66131ad4be811
Marvin <you@example.com>
2018-03-28 09:58:52 +1000

    exerimental change, only do work verification for packages that we have SEEN being submitted

--- src/main/java/org/xel/computation/CommandNewWork.java
+++ src/main/java/org/xel/computation/CommandNewWork.java
@@ -222,6 +222,8 @@ public class CommandNewWork extends IComputationAttachment {
     @Override
     boolean validate(Transaction transaction) {
 
+        if(transaction.getTimestamp() < Nxt.getEpochTime()-5*60) return false;
+
 
         if (((this.sourceCode == null) || (this.sourceCode.length == 0)) && (this.sourceCodeCompressed == null) || (this.sourceCodeCompressed.length == 0)) return false;
 
