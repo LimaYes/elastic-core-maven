f2644ff79cedd4701ecbfe38232f768056c63095
Starfish <daniel.cagara@googlemail.com>
2018-04-10 21:37:42 +1000

    versionbump

--- src/main/java/org/xel/Nxt.java
+++ src/main/java/org/xel/Nxt.java
@@ -50,7 +50,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "5.0.0";
+    public static final String VERSION = "5.1.0";
     public static final String APPLICATION = "XEL Evo";
 
     private static volatile Time time = new Time.EpochTime();
