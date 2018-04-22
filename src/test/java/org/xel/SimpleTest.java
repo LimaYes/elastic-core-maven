9060bd2a4ed895e7c56e38d31248e74949dc91d5
Starfish <None.of.your@busine.ss>
2018-04-23 07:15:50 +1000

    PATH FIX

---
+++ src/test/java/org/xel/SimpleTest.java
@@ -0,0 +1,25 @@
+package org.xel;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.xel.computation.CommandPowBty;
+import org.xel.computation.IComputationAttachment;
+import org.xel.computation.MessageEncoder;
+
+import java.util.Properties;
+
+public class SimpleTest extends AbstractForgingTest {
+
+    protected static boolean isNxtInitted = false;
+
+
+    @Test
+    public void testStrangeBlock(){
+        System.out.println(Constants.EPOCH_BEGINNING);
+    }
+
+
+
+}
