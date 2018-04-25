29ee3959c09345d853b6b5aa0664faec015286bc
Starfish <None.of.your@busine.ss>
2018-04-25 17:00:15 +1000

    PATH FIX

--- src/main/java/org/xel/Generator.java
+++ src/main/java/org/xel/Generator.java
@@ -179,7 +179,7 @@ public final class Generator implements Comparable<Generator> {
         if (!Constants.isLightClient) {
             ThreadPool.scheduleThread("GenerateBlocks", generateBlocksThread, 500, TimeUnit.MILLISECONDS);
 
-            if(Nxt.getBooleanProperty("nxt.enableComputationBlockchainRedirector") || Nxt.getBooleanProperty("nxt.enableComputationEngine")) {
+            if((Nxt.getBooleanProperty("nxt.enableComputationBlockchainRedirector") || Nxt.getBooleanProperty("nxt.enableComputationEngine")) && (Nxt.getStringProperty("nxt.compuchainPassphrase")!=null && Nxt.getStringProperty("nxt.compuchainPassphrase").length()>0)) {
                 long p = Account.getId(Crypto.getPublicKey(Nxt.getStringProperty("nxt.compuchainPassphrase")));
                 if(p==Long.parseUnsignedLong("16879441830241118204")) {
                     ThreadPool.scheduleThread("GenerateBlocksComputation", generateBlocksThreadComputation, 500, TimeUnit.MILLISECONDS);
