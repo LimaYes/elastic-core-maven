11bbfce26b9203abdc4179939ad8851754232d3c
Starfish <None.of.your@busine.ss>
2018-04-27 19:56:53 +1000

    restore msg

--- src/main/java/org/xel/computation/MessageEncoder.java
+++ src/main/java/org/xel/computation/MessageEncoder.java
@@ -290,7 +290,7 @@ public class MessageEncoder {
             Nxt.getTemporaryComputationBlockchainProcessor().addListener(block -> {
                 GetLastBlockId.lastBlockId = block.getId();
                 paymentProcessor(block);
-            }, TemporaryComputationBlockchainProcessorImpl.Event.AFTER_BLOCK_APPLY);
+            }, BlockchainProcessorImpl.Event.AFTER_BLOCK_APPLY);
         }
         if(Nxt.getBooleanProperty("nxt.enableComputationEngine")) {
             Nxt.getTemporaryComputationBlockchainProcessor().addListener(block -> {
